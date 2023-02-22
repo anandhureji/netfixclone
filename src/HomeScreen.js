d4f3afb7d15525223b244d3d2316ac7668a0b3da
Reji <2111896@cognizant.com>
2023-02-22 15:24:01 +0530

    Added nav bar and its css

---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const HomeScreen = () => {
+  return (
+    <div className='homescreen'>
+    <Navbar />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+    <div className='nav_content'>
+    <img className='img_logo' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"  alt=''/>
+    <img className='img_avathar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt='' />
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
