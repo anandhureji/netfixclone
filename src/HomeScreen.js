ec48130fd96fe7261e57f893e42f7e22c42ae7b8
Reji <2111896@cognizant.com>
2023-02-23 11:13:29 +0530

    added navbar

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Navbar from './Navbar'
+import  './HomeScreen.css'
 
 const HomeScreen = () => {
   return (
     <div className='homescreen'>
     <Navbar />
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
+    <h1>movie1</h1>
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 const Navbar = () => {
+    const [show,handleShow] = useState(false);
+    const transitionNavbar =() =>{
+        if(window.screenY>100){
+            handleShow(true)
+        }
+        else{
+            handleShow(false)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll",transitionNavbar);
+        return ()=>window.removeEventListener("scroll",transitionNavbar);
+    },[]
+    )
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav_black"}`}>
     <div className='nav_content'>
-    <img className='img_logo' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"  alt=''/>
+    <img className='img_logo' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"  alt=''/>
     <img className='img_avathar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt='' />
     </div>
     </div>
+    
   )
 }
 
